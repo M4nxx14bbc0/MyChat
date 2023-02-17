8aaae8351ff3f01374aa9a564219dc552ca65923
M4nxx14bbc0 <parrarodriguez.manue@studenti.salesianisesto.it>
2023-02-17 11:25:13 +0100

    created front-end

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server,{
 });
 
 io.on('connection', client =>{
-    client.on('event',message=>{
+    client.on('message',message=>{
         console.log(message);
         io.emit('message', client.id.substr(0,2)+' said '+message)
     });
